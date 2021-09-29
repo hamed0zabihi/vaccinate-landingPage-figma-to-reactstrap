3ffb05d1b9de04fefb9d29b19504307832a52916
hamed zabihi <hamed0zabihi@gmail.com>
2021-09-29 14:20:23 +0330

    :lipstick: HeaderSection-add font,logo and so on

--- src/component/landing/header/HeaderSection.jsx
+++ src/component/landing/header/HeaderSection.jsx
@@ -1,5 +1,51 @@
+import React, { useState } from "react";
+import {
+  Container,
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from "reactstrap";
+
 const HeaderSection = () => {
-  return <></>;
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  return (
+    <header>
+      <Container>
+        <div>
+          <Navbar expand="md">
+            <NavbarBrand href="/">
+              <img src="./img/logo/logo.svg" alt="logo" />
+            </NavbarBrand>
+            <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
+              <Nav navbar className="ms-auto">
+                <NavItem>
+                  <NavLink href="/components/">about us</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/#">learning the vaccine</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/#">apps</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/#">service</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/#">blog</NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </div>
+      </Container>
+    </header>
+  );
 };
 
 export default HeaderSection;
